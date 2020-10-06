5d32a6fc6efcce0928a4420e27131a548d7b8f4e
DortegaO <danielortega.o@gmail.com>
2020-10-06 20:04:45 -0300

    Comit inicial

---
+++ app/src/main/java/com/example/fittnes_app/Clientes_act.java
@@ -0,0 +1,60 @@
+package com.example.fittnes_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class Clientes_act extends AppCompatActivity {
+
+    private Spinner spin1,spin2;
+    private TextView text;
+    private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clientes_act);
+
+        spin1 = (Spinner)findViewById(R.id.spin_clientes);
+        spin2 = (Spinner)findViewById(R.id.spiner_planes);
+        text = (TextView) findViewById(R.id.tv);
+        edit = (EditText) findViewById(R.id.et1);
+
+        ArrayList<String> listaClientes = (ArrayList<String>)getIntent().getSerializableExtra("listaClientes");
+        ArrayList<String> listaplanes = (ArrayList<String>)getIntent().getSerializableExtra("listaPlanes");
+
+
+        ArrayAdapter<String> adap = new ArrayAdapter(this,android.R.layout.simple_list_item_1,listaClientes);
+        ArrayAdapter<String> adaps = new ArrayAdapter(this,android.R.layout.simple_list_item_1,listaplanes);
+
+        spin1.setAdapter(adap);
+        spin2.setAdapter(adaps);
+    }
+
+    public void Calcular(View v)
+    {
+        String cliente = spin1.getSelectedItem().toString();
+        String planes = spin2.getSelectedItem().toString();
+
+        if(cliente.equals("roberto") && planes.equals("normal"))
+        {
+            text.setText("el valor es");
+
+        }
+
+
+
+
+    }
+
+
+}
